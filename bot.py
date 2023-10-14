b3b55b8335135f640a9f13f4243986296add49ff
riken-khadela <rikenkhadelamain@gmail.com>
2023-10-14 17:12:04 +0530

    fa f

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ class scrapping_bot():
         options.add_argument("--ignore-gpu-blocklist")
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--headless')
-        options.add_argument("--user-data-dir=/home/sajal/.config/google-chrome")
+        options.add_argument("--user-data-dir=/home/dell/.config/google-chrome")
         options.add_argument('--profile-directory=Default')
         prefs = {"credentials_enable_service": True,
                  "download.default_directory" : "./downloads",
@@ -491,9 +491,9 @@ class scrapping_bot():
                     
                     import os
                     if not os.path.exists(os.path.join(os.getcwd(),'videos')) : os.makedirs(os.path.join(os.getcwd(),'videos'))
-                    with open(os.path.join(os.getcwd(),f'videos/{video_name}.mp4'), 'w') as fp:  pass
+                    # with open(os.path.join(os.getcwd(),f'videos/{video_name}.mp4'), 'w') as fp:  pass
 
-                    m3u8_To_MP4.multithread_download(master_url[0])
+                    m3u8_To_MP4.multithread_download(master_url[0],mp4_file_name=video_name,mp4_file_dir= os.path.join(os.getcwd(),'videos'))
                     self.videos_collection.append(tmp)
                     self.videos_data.append({ "Video-title" : video_name,"video_url" : videoss_urll['video_url'],"downloaded_time" : datetime.now()})
 
