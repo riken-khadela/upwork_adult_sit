33f82a4650fe786d369d8022d29ef39063d4ce21
riken-khadela <rikenkhadelamain@gmail.com>
2023-10-17 17:03:59 +0530

    admin url change

--- scrapping/urls.py
+++ scrapping/urls.py
@@ -51,7 +51,7 @@ def csv_file(request):
         return HttpResponse("File not found", status=404)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('downloads/<path:file_path>/', download_file, name='download_file'),
     path('list_files/', list_files, name='list_file'),
     path('csv/', csv_file, name='csv_file')
