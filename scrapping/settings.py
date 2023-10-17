b27eff75482a97f71f1a390e2b13d049688877a9
riken-khadela <rikenkhadelamain@gmail.com>
2023-10-17 15:28:21 +0530

    dd

--- scrapping/settings.py
+++ scrapping/settings.py
@@ -119,9 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 MEDIA_URL = '/downloads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'downloads')
