0b32ac076f9cfe3cb776ff966e0c82af316c5696
riken-khadela <rikenkhadelamain@gmail.com>
2023-10-17 16:30:25 +0530

    static gitignore

--- scrapping/settings.py
+++ scrapping/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
