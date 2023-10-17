5787bc5c0dce2c75d377cb23082171404573872a
riken-khadela <rikenkhadelamain@gmail.com>
2023-10-17 11:52:17 +0530

    complete 17 11:52

--- scrapping/urls.py
+++ scrapping/urls.py
@@ -1,22 +1,25 @@
-"""
-URL configuration for scrapping project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from django.views.generic import View
+from django.http import HttpResponse
+from django.conf import settings
+import os
+from django.contrib import admin
+
+def download_file(request, file_path):
+    # Construct the full path to the file using the MEDIA_ROOT setting.
+    media_root = settings.MEDIA_ROOT
+    file = os.path.join(media_root, file_path)
+
+    # Check if the file exists.
+    if os.path.isfile(file):
+        with open(file, 'rb') as f:
+            response = HttpResponse(f.read(), content_type="application/octet-stream")
+            response['Content-Disposition'] = f'attachment; filename="{os.path.basename(file)}"'
+            return response
+    else:
+        return HttpResponse("File not found", status=404)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('downloads/<path:file_path>/', download_file, name='download_file'),
 ]
