62734ae900037aa89e131861d5e838218a201140
riken-khadela <rikenkhadelamain@gmail.com>
2023-10-17 11:58:10 +0530

    complete 17 11:58

--- scrapping/urls.py
+++ scrapping/urls.py
@@ -5,6 +5,22 @@ from django.conf import settings
 import os
 from django.contrib import admin
 
+
+def list_files(request):
+    # Get the path to your STATIC_ROOT directory
+    static_root = settings.MEDIA_ROOT
+
+    # List all files in the directory
+    files = os.listdir(static_root)
+
+    # Create an HTML response with links to the files
+    file_links = []
+    for file in files:
+        if file.endswith('.mp4'):
+            file_path = os.path.join(static_root, file)
+            file_links.append(f'<a href="/downloads/{file}/">{file}</a>')
+    
+    return HttpResponse("<br>".join(file_links))
 def download_file(request, file_path):
     # Construct the full path to the file using the MEDIA_ROOT setting.
     media_root = settings.MEDIA_ROOT
@@ -22,4 +38,5 @@ def download_file(request, file_path):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('downloads/<path:file_path>/', download_file, name='download_file'),
+    path('list_files/', list_files, name='list_file'),
 ]
