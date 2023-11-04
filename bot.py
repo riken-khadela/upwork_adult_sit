746582f8f1648b5faece861b04b98d9a1edc6386
riken-khadela <rikenkhadelamain@gmail.com>
2023-11-04 14:10:57 +0530

    remove brakpoint

--- bot.py
+++ bot.py
@@ -30,7 +30,6 @@ class scrapping_bot():
             self.downloaded_videos_list = os.listdir('downloads')
             self.videos_urls = []
             self.brazzers_category_url = 'https://site-ma.brazzers.com/categories'
-        breakpoint()
 
     def get_driver(self,add_cybeghost=False):
         """Start webdriver and return state of it."""
