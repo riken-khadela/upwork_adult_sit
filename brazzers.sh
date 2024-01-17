70572fbd25da8c69c9a5b2936c98b4b775c6a856
riken-khadela <rikenkhadelamain@gmail.com>
2024-01-17 16:58:24 +0530

    hddh

--- brazzers.sh
+++ brazzers.sh
@@ -1,7 +1,29 @@
-VENV_ACTIVATE="/home/dell/Desktop/upwork/brazzers/env/bin/activate"
+# project path
+export CURRENT_DIR=`dirname $(readlink -f $0)`
+export PRJ_DIR=`dirname $CURRENT_DIR`
+# go to project root directory
+cd $PRJ_DIR
+#. ./tasks/environment.sh
+# . tasks/environment.sh
+# . tasks/variable.sh
 
-# Activate the virtual environment
-source "$VENV_ACTIVATE"
+killall -9 python qemu-system-x86_64
+# Kill python and AVD process
+# export SENDER_PASSWORD='hfac mvld ecjx clru'
+# export RECEIVER_MAIL="rikenkhadela22@gmail.com"
+# export SENDER_MAIL='rikenkhadela777@gmail.com'
+# export SYSTEM_NO='RK'
+# activate the virtual environment for python
+#. env/bin/activate
+. env/bin/activate
 
-# Run your Python script
-python /home/dell/Desktop/upwork/brazzers/brazzers.py
+# update code
+# git checkout old-insta-rk
+# git stash
+# git pull 
+
+# setup database
+# python temp/z22.py
+# python manage.py update_csv 
+# python manage.py delete_avd 
+python manage.py update_conf
